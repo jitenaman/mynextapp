a13fea40d77911008b0c83d5caa7a4c7af39cb46
jitenaman <jitendra.weblink@gmail.com>
2025-04-22 21:41:06 +0530

    Update contact.ts

--- src/Constants/contact.ts
+++ src/Constants/contact.ts
@@ -1,6 +1,6 @@
 export const footerContent = {
   title: "Let&apos;s make your day memmorable",
-  companyName: "Wedding Company Delhi",
+  companyName: "Company Delhi",
   // each item will be in new line
   // address:[
   // {id:"addressline1",lineContent:"Delhi, India"},
@@ -9,14 +9,14 @@ export const footerContent = {
   // ]
   address: [{ id: "addressLine1", lineContent: "Delhi, India" }],
   contacts: {
-    email: "weddingcompanydelhi23@gmail.com",
-    mobile: "+91 9947176844",
-    whatsapp: "919947176844", // keep the same format 91XXXXXXXXXX
+    email: "test@gmail.com",
+    mobile: "+91 0000000000",
+    whatsapp: "910000000000", // keep the same format 91XXXXXXXXXX
   },
   socialLinks: {
-    instagram: "https://www.instagram.com/weddingcompanydelhi/",
-    youtube: "https://www.youtube.com/@weddingcompanydelhi",
-    threads: "https://www.threads.net/@weddingcompanydelhi",
-    facebook: "https://www.facebook.com/WeddingCompanyDelhi/",
+    instagram: "https://www.instagram.com/",
+    youtube: "https://www.youtube.com/",
+    threads: "https://www.threads.net/",
+    facebook: "https://www.facebook.com/",
   },
 };
